a20569c75093c0abb6d69b3a3fa503a2cfa544c9
jaegarpoon <73332327+jaegarpoon@users.noreply.github.com>
2022-03-29 19:49:22 +0800

    Add list order functionality

---
+++ src/main/java/seedu/address/logic/parser/ListOrderCommandParser.java
@@ -0,0 +1,66 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import seedu.address.logic.commands.ListOrderCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.order.OrderStatus;
+import seedu.address.model.order.OrderStatusContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new ListOrderCommand object
+ */
+public class ListOrderCommandParser implements Parser<ListOrderCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListOrderCommand
+     * and returns a ListOrderCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListOrderCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListOrderCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        if (nameKeywords.length > 1) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListOrderCommand.MESSAGE_USAGE));
+        }
+
+        if (!orderStatusHasKeyword(nameKeywords[0])) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListOrderCommand.MESSAGE_USAGE));
+        }
+
+        return new ListOrderCommand(new OrderStatusContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+    /**
+     * Checks the given {@code String} argument and
+     * returns a boolean representing if enum OrderStatus contains argument or
+     * argument is equal to "all" (not case-sensitive)
+     */
+    private boolean orderStatusHasKeyword (String keyword) {
+        if (keyword.toUpperCase().equals("ALL")) {
+            return true;
+        }
+        boolean hasKeyword = false;
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.name().equals(keyword.toUpperCase())) {
+                hasKeyword = true;
+                break;
+            }
+        }
+
+        return hasKeyword;
+    }
+
+}
+
